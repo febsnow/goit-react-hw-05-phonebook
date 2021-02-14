e94221cb1f60ddcd90c7b8bd7b9404f699540d9d
Smekalin Sergei <crypto.mnr@gmail.com>
2021-02-14 19:20:29 +0200

    small fix

--- src/сomponents/ErrorPrompt/ErrorPrompt.jsx
+++ src/сomponents/ErrorPrompt/ErrorPrompt.jsx
@@ -5,7 +5,7 @@ import styles from "./ErrorPrompt.module.css";
 const ErrorPrompt = ({ message }) => {
   return (
     <CSSTransition appear={true} in={true} timeout={250} classNames={styles}>
-      <div className={styles.errorPrompt}>
+      <div className={styles.errorPrompt} unmountOnExit>
         <p>{message}</p>
       </div>
     </CSSTransition>
